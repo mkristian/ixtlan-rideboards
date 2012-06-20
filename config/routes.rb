0011ae459b0777c4961c3d720a246671f4bcd958
Kristian Meier <m.kristian@web.de>
2012-06-20 19:07:06 +0530

    get the routing migrated

--- config/routes.rb
+++ config/routes.rb
@@ -11,61 +11,34 @@ Rideboard::Application.routes.draw do
     end
   end
 
-
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
   #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+  # public students' UI from here on
+  #
+  scope ":lang" do
+    scope ":venue" do
+      scope ":board" do
+        resources :listings, :module => "public" do
+          collection do
+            get :wanted
+            get :offer
+          end
+          post :reminder, :on => :member
+          resource :contact
+        end
+      end
+    end
+  end
 
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+  # named venue and named board with language choice
+  match ":lang/:venue/:board" => "public/boards#show"
+  match ":lang/:venue/:board/index" => "public/boards#show"
 
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
+  # named venue with language choice
+  match ":lang/:venue" => "public/boards#lang_venue"
+  match ":lang/:venue/index" => "public/boards#lang_venue"
 
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  # named venue with language negotiation
+  match ":venue" => "public/boards#venue"
+  match ":venue/index" => "public/boards#venue"
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
 end
