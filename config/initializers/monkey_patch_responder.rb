570f06091a8d5f8c95e5a03fdf2db20728042644
Kristian Meier <m.kristian@web.de>
2012-06-21 21:10:17 +0530

    responder patch which workd for all rails-3.x.y

--- config/initializers/monkey_patch_responder.rb
+++ config/initializers/monkey_patch_responder.rb
@@ -1,13 +1,25 @@
 module ActionController
   class Responder
-    def empty_json_resource
-      resource.to_json
-    end
-    def empty_xml_resource
-      resource.to_xml
-    end
-    def empty_yml_resource
-      resource.to_yml
+    # def empty_json_resource
+    #   resource.to_json
+    # end
+    # def empty_xml_resource
+    #   resource.to_xml
+    # end
+    # def empty_yml_resource
+    #   resource.to_yml
+    # end
+
+    def api_behavior(error)
+      raise error unless resourceful?
+
+      if get? || put?
+        display resource
+      elsif post?
+        display resource, :status => :created, :location => api_location
+      else
+        head :no_content
+      end
     end
   end
 end
