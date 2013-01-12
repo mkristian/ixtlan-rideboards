f215b84d5083e45a0d027b596ab01bf94bfa3a6c
Christian Meier <m.kristian@web.de>
2013-01-12 22:46:29 +0530

    switch missing pieces to pony

--- config/initializers/emails.rb
+++ config/initializers/emails.rb
@@ -1,13 +1,13 @@
-# see also ../environment/developement.rb ../environment/test.rb
-
 if ENV['SENDGRID_USERNAME']
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com'
+  Pony.options = {
+    :via         => :smtp,
+    :via_options => {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV[ 'SENDGRID_USERNAME' ],
+      :password       => ENV[ 'SENDGRID_PASSWORD' ],
+      :domain         => 'heroku.com'
+    }
   }
-  ActionMailer::Base.delivery_method = :smtp
 end
